ed0a81bed6dbc1dc3803a2182001668206861428
Procodersuo <100094332+Procodersuo@users.noreply.github.com>
2024-10-25 16:12:47 +0500

    app bar updated // disappering on other tabs done

--- lib/colors/colors.dart
+++ lib/colors/colors.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 class MyColors {
  static Color primaryColor=const Color(0xFF3b5198);
+ static Color backGroundColor = Colors.white;
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double height = MediaQuery.of(context).size.height;
+    return const CustomScrollView(
+      slivers: [
+
+      ],
+    );
   }
 }
