1197fd42db765b6c068ae2cdeec8e93cda33c1eb
Procodersuo <100094332+Procodersuo@users.noreply.github.com>
2024-10-27 20:12:11 +0500

    Stuff added in notifications_screens.dart screen

--- lib/screens/notifications_screens.dart
+++ lib/screens/notifications_screens.dart
@@ -2,6 +2,8 @@ import 'package:facebook_ui_design/widgets/like_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../colors/colors.dart';
+
 class NotificationsScreens extends StatelessWidget {
   const NotificationsScreens({super.key});
 
@@ -19,16 +21,68 @@ class NotificationsScreens extends StatelessWidget {
           ],
         ),
         // Use SliverList instead of ListView
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-              return const ListTile(
-                title: Text("HELLO"),
+const SliverToBoxAdapter(
+  child: Padding(
+    padding: EdgeInsets.all(8.0),
+    child: Text("Today",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 18),),
+  ),
+),
+        SliverList(delegate: SliverChildBuilderDelegate(
+                (BuildContext context,int index)
+            {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                    leading: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: MyColors.storiesColors[index],
+                          shape: BoxShape.circle
+                      ),
+                    ),
+                    title: const Text("Ali send you friend Request"),
+                    subtitle: Text("111483948d"),
+                    trailing:const Icon(Icons.more_horiz)
+
+                ),
               );
             },
-            childCount: 20, // Number of items
+            childCount: 4
+        )),
+        const SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text("Earlier",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 18),),
           ),
         ),
+        SliverList(delegate: SliverChildBuilderDelegate(
+                (BuildContext context,int index)
+            {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    ListTile(
+                        leading: Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                              color: MyColors.storiesColors[index],
+                              shape: BoxShape.circle
+                          ),
+                        ),
+                        title: const Text("Ahmad invites you to join his Channel"),
+                        subtitle: const Text("300000d"),
+                        trailing:const Icon(Icons.more_horiz)
+
+                    ),
+                  ],
+                ),
+              );
+            },
+            childCount: 4
+        )),
       ],
     );
   }
